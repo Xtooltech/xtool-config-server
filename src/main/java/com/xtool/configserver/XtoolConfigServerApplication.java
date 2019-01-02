8d94d4828460754d60077841c8cd65f7f174e1ec
Siuhua Lee <xiaohua.li@xtooltech.com>
2019-01-02 22:00:36 +0800

    first commit by lxh

---
+++ src/main/java/com/xtool/configserver/XtoolConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.xtool.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class XtoolConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtoolConfigServerApplication.class, args);
+	}
+}
